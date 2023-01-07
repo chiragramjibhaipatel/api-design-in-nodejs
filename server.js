e8d23284f14d6eb0483d6f8f4ab23e2ecab659b6
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2023-01-07 11:10:36 +0530

    Node.js -> Exercise 1 Solution

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require("express");
+const path = require("path");
 
 var app = express();
 let todos = [];
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'), function (err){
+    res.status(500).send("Not Found");
+  });
+})
 app.get("/todos", (req, res) => {
   //   sometimes we get 304 on client: more details here: https://stackoverflow.com/questions/18811286/nodejs-express-cache-and-304-status-code
   //   res.header("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -11,3 +18,4 @@ app.get("/todos", (req, res) => {
 });
 
 app.listen(3000);
+console.log("listening on PORT 3000");
